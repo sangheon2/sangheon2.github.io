a03c77d09153dcb197c6219f7d2d67a0b5b3c966
Sangheon Jeon <94092155+sangheon2@users.noreply.github.com>
2026-03-01 18:24:31 -0800

    Update DynamicPageClient.tsx

--- src/components/pages/DynamicPageClient.tsx
+++ src/components/pages/DynamicPageClient.tsx
@@ -31,7 +31,7 @@ export default function DynamicPageClient({ dataByLocale, defaultLocale }: Dynam
   }
 
   return (
-    <div className="mx-auto max-w-[1400px] px-4 sm:px-6 lg:px-8 py-12">
+    <div className="max-w-7xl mx-auto] px-4 sm:px-6 lg:px-8 py-12">
       {pageData.type === 'publication' && (
         <PublicationsList config={pageData.config} publications={pageData.publications} />
       )}
